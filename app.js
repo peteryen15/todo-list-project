f78cd954850bc46c56d38a143869d543531ab9ee
peteryen15 <peteryen15@gmail.com>
2022-08-16 11:41:04 +0800

    加入 Local Storage 功能

--- app.js
+++ app.js
@@ -51,12 +51,92 @@ add.addEventListener("click", (e) => {
   deleteButton.innerText = "Delete";
   deleteButton.addEventListener("click", (e) => {
     let todoItem = e.target.parentElement;
+
+    // remove from localStorage
+    let text = todoItem.children[0].innerText;
+    let newListArray = JSON.parse(localStorage.getItem("list"));
+    newListArray.forEach((item, index) => {
+      if (item.todoText == text) {
+        newListArray.splice(index, 1);
+        localStorage.setItem("list", JSON.stringify(newListArray));
+      }
+    });
+
     todoItem.remove();
   });
 
   todoDiv.appendChild(completeCheckBox);
   todoDiv.appendChild(deleteButton);
 
+  let todoObj = {
+    todoText: todoText,
+    todoMonth: todoMonth,
+    todoDay: todoDay,
+  };
+
+  // store data into localStorage
+  let getList = localStorage.getItem("list");
+  if (getList == null) {
+    localStorage.setItem("list", JSON.stringify([todoObj]));
+  } else {
+    let newListArray = JSON.parse(getList);
+    newListArray.push(todoObj);
+    localStorage.setItem("list", JSON.stringify(newListArray));
+  }
+
   form.children[0].value = "";
   section.appendChild(todoDiv);
 });
+
+let getList = localStorage.getItem("list");
+if (getList !== null) {
+  let newListArray = JSON.parse(getList);
+
+  newListArray.forEach((item) => {
+    // create todo
+    let todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    let textP = document.createElement("p");
+    textP.classList.add("todo-text");
+    textP.innerText = item.todoText;
+    let timeP = document.createElement("p");
+    timeP.classList.add("todo-time");
+    timeP.innerText = item.todoMonth + " / " + item.todoDay;
+    todoDiv.appendChild(textP);
+    todoDiv.appendChild(timeP);
+
+    // create checkbox
+    let completeCheckBox = document.createElement("input");
+    completeCheckBox.setAttribute("type", "checkbox");
+    completeCheckBox.classList.add("complete");
+    completeCheckBox.addEventListener("change", (e) => {
+      let todoItem = e.target.parentElement;
+      todoItem.classList.toggle("done");
+    });
+
+    // create delete button
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", (e) => {
+      let todoItem = e.target.parentElement;
+
+      // remove from localStorage
+      let text = todoItem.children[0].innerText;
+      let newListArray = JSON.parse(localStorage.getItem("list"));
+      newListArray.forEach((item, index) => {
+        if (item.todoText == text) {
+          newListArray.splice(index, 1);
+          localStorage.setItem("list", JSON.stringify(newListArray));
+        }
+      });
+
+      todoItem.remove();
+    });
+
+    todoDiv.appendChild(completeCheckBox);
+    todoDiv.appendChild(deleteButton);
+
+    section.appendChild(todoDiv);
+  });
+}
